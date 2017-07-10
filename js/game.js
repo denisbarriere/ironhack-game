f618f5549045e069da227ab625ed5ae9300a903d
Denis Barriere <denis.barriere@gmail.com>
2017-07-10 19:19:53 +0200

    Split game-logic.js in 3 JS files. Created the following function to manage proper positionning: Level.setOIconPosition, Level.setPlusIconPosition and Level.setXIconPosition

---
+++ js/game.js
@@ -0,0 +1,36 @@
+/******************************
+ *                            * 
+ *  OBJECT: Game              *
+ *    => Constructor          *
+ *                            *
+ * ****************************/
+function Game() {
+  
+  // Initialise the Player Object
+  this.player = new Player();
+
+  // Initialise the Level Object to the first level for a start
+  this.level = new Level(level1Properties);
+  this.level.initLevel(this.player);
+
+}
+
+
+/******************************
+ *                            * 
+ *  OBJECT: Game              *
+ *    => Functions            *
+ *                            *
+ * ****************************/
+
+/**
+ * FUNCTION: Call all the required functions to resize the game
+ * PARAMETERS: None
+ * RETURNS: undefined
+ * DEPENDENCIES: + Level.setBoardHeaderSize()
+ *               + Level.setBoardSize()
+**/
+Game.prototype.resize = function() {
+  this.level.setBoardHeaderSize();
+  this.level.setBoardSize();
+};
